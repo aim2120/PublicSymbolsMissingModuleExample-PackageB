31f769d07f2b94e51d4f13ab561cc6da200a4cea
Annalise Mariottini <annalise.mariottini@columbia.edu>
2025-05-06 18:15:25 -0400

    change to ObjC package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PublicSymbolsMissingModuleExample-PackageB"),
+            name: "PublicSymbolsMissingModuleExample-PackageB",
+            publicHeadersPath: "Public",
+            cSettings: [
+                .headerSearchPath("Public")
+            ]
+        ),
         .testTarget(
             name: "PublicSymbolsMissingModuleExample-PackageBTests",
             dependencies: ["PublicSymbolsMissingModuleExample-PackageB"]
